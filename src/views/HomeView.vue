48367bcb0621d91dbfe3e35999afc45134bfb1dd
Young rich <tajiriramadhan@gmail.com>
2024-03-24 07:50:10 +0300

    finishing the computed finishing serch project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,62 +1,30 @@
 <template>
   <div class="home">
-    <p ref="para">My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">Click Me</button>
-    <hr>
-    <button @click="age++">add 1 to age</button>
-    <hr>
-    <input type="text" v-model="name">
-    <hr>
-    <div>
-      <p>My name is {{ ninja.name }} and my age is {{ ninja.age }}</p>
+   <h1>Home</h1>
+   <input type="text" v-model="search">
+   <p>serch term - {{ search }}</p>
 
-      <button @click="updateNinja">update ninja</button>
-    </div>
+   <div v-for="name in matchingNames" :key="name"> {{ name }}</div>
   </div>
 </template>
 
 <script>
-import { ref, reactive } from 'vue'
+import { ref, reactive,computed } from 'vue'
 
 // @ is an alias to /src
 export default {
   name: 'HomeView',
   setup() {
-    console.log('setup')
-//non reactive variables
-    // let name = 'Tajir'
-    // let age  = 70
+    const search = ref('')
+    const names = ref(['mario', 'yoshi', 'Luigi', 'bowser','toad','koopa', 'peach','Tajir'])
 
-    //reactive variables
-    const name = ref('Tajir')
-    const age = ref(60)
+    const matchingNames = computed(
+      () => {
+        return names.value.filter( (name) => name.includes(search.value))
+      }
+    )
 
-    const handleClick = () => {
-      console.log(para, para.value)
-      para.value.classList.add('taji')
-
-      //Updating the refs values
-      name.value = 'Don Diego'
-      age.value = 70
-    }
-
-    const para = ref(null)
-
-    const ninja = reactive({ name: 'Bar Ba Rossa', age: 60})
-
-    const updateNinja = () => {
-      ninja.name = "Mama Mzazi"
-      ninja.age = 11
-    }
-
-    return{
-      name,
-      age,
-      handleClick,
-      para,
-      ninja,
-      updateNinja
-    }
+    return { names, search, matchingNames }
   }
 }
 </script>
