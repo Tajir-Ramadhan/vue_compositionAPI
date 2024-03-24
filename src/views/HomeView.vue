6f42fd0bf3e92edc388d55cfe1c39f9f488876dd
Young rich <tajiriramadhan@gmail.com>
2024-03-24 07:08:51 +0300

    starting composition api using non reactive variables

---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <p ref="para">My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">Click Me</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+// @ is an alias to /src
+export default {
+  name: 'HomeView',
+  setup() {
+    console.log('setup')
+
+    let name = 'Tajir'
+    let age  = 70
+
+    const handleClick = () => {
+      console.log(para, para.value)
+      para.value.classList.add('taji')
+      name = 'Don Diego'
+    }
+
+    const para = ref(null)
+
+    return{
+      name,
+      age,
+      handleClick,
+      para
+    }
+  }
+}
+</script>
