6b5bf5f87ad774f16927ef73ce59450ea8ec31a2
Young rich <tajiriramadhan@gmail.com>
2024-03-25 21:11:58 +0300

    finishing vue totorials fetch composition part

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,33 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <posts :posts="posts"/>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <posts :posts="posts"/>
+    </div>
+    <div v-if="!posts.length && !error">
+      <p>Loading ......</p>
+    </div>
   </div>
  
 </template>
 
 <script>
-import { ref} from 'vue'
+
 import Posts from '../components/Posts.vue'
+import getPosts from '../composables/getPosts'
 
 // @ is an alias to /src
 export default {
   name: 'HomeView',
   components: { Posts },
   setup() {
-   const posts = ref([
-    {title: 'Welcome to the blog', body: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nesciunt adipisci illo quis, cupiditate cumque dicta quod perferendis! Temporibus dolorum eaque, ipsam nam minima, nobis velit nisi sunt aperiam reprehenderit ipsa non aliquid ut vitae labore sit corrupti! Ipsum ut sapiente eum consequatur id nostrum reprehenderit, expedita assumenda, soluta veniam tempore deleniti harum asperiores nisi. Repudiandae sapiente ex modi architecto voluptas veritatis sunt. Possimus fugit voluptatum officiis labore, tempore sunt eligendi minima! Maxime laboriosam unde enim corrupti iusto nulla, architecto mollitia rem veniam, itaque distinctio? Facere aliquam sapiente magnam quis commodi libero, omnis eligendi placeat sunt, qui ullam voluptatum architecto.', id: 1},
-    {title: 'Top 5 CSS tips', body: 'Lorem ipsum', id: 2},
-   ])
 
-   return {
-    posts
-   }
+   const {posts, error, load} = getPosts()
+
+   load()
+
+   return { posts, error }
   }
 }
 </script>
